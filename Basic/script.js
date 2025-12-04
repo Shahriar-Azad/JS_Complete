e57817463cd65606522912eefa03de737295d512
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-12-04 19:06:29 +0600

    arithmatic operations

--- Basic/script.js
+++ Basic/script.js
@@ -30,13 +30,34 @@
 // console.log(isSale);
 
 
-let name = "Shihab";
-let age = 20;
-let mail = "shihab123@gmail.com";
+// let name = "Shihab";
+// let age = 20;
+// let mail = "shihab123@gmail.com";
 
 
-document.getElementById('p1').textContent = `My name is ${name}`;
-document.getElementById('p2').textContent = `My age is ${age}`;
-document.getElementById('p3').textContent = `My email is ${mail}`;
+// document.getElementById('p1').textContent = `My name is ${name}`;
+// document.getElementById('p2').textContent = `My age is ${age}`;
+// document.getElementById('p3').textContent = `My email is ${mail}`;
+
+// arithmatic operations
+
+let age = 10;
+console.log(age += 1);
+console.log(age -=1 );
+console.log(age *= 2);
+console.log(age /= 2);
+
+
+// operator precendences
+
+// (), exponents, multiplication, divison, modulus, addition, substraction
+
+let result = 1 + (2 * 4) + 7 - (3 * 5) - (8 ** 2);
+
+console.log(result)
+
+
+
+
 
 
