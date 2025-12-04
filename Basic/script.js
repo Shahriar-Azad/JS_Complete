7359057dea43a99c8e628e66407ac658f2e70e26
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-12-04 18:48:31 +0600

    normal variables added

--- Basic/script.js
+++ Basic/script.js
@@ -1,9 +1,20 @@
-console.log(hello);
+// console.log(hello);
 
-function add(a,b){
-    return a+b;
+// function add(a,b){
+//     return a+b;
 
-}
+// }
 
 
-add(10,20);
+// add(10,20);
+
+// variables
+
+let name = "shahriar";
+let age = 24;
+let email = "shahriar.azad123@gmail.com";
+
+
+console.log(`My name is ${name}`);
+console.log(`My age is ${age}`);
+console.log(`My email is ${email}`);
