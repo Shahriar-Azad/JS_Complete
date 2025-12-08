61e6c39f975c2a1c9dfee7fe65051316e8cca560
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-12-08 23:40:46 +0600

    one liner rps

--- Rock-Paper-Scissor/script.js
+++ Rock-Paper-Scissor/script.js
@@ -33,23 +33,28 @@
 // scissor.addEventListener("click",()=> playGame("scissor"));
 
 
-const choices = ["rock", "paper", "scissor"];
+// const choices = ["rock", "paper", "scissor"];
 
-function playGame(userChoice) {
-  const comp = choices[Math.floor(Math.random()*3)];
-  document.getElementById("myH1").textContent = `Computers Choice: ${comp}`;
+// function playGame(userChoice) {
+//   const comp = choices[Math.floor(Math.random()*3)];
+//   document.getElementById("myH1").textContent = `Computers Choice: ${comp}`;
 
-  const result =
-    userChoice === comp ? "It's a draw" :
-    (userChoice === "rock" && comp === "scissor") ||
-    (userChoice === "paper" && comp === "rock") ||
-    (userChoice === "scissor" && comp === "paper")
-    ? "You won" : "You lose";
+//   const result =
+//     userChoice === comp ? "It's a draw" :
+//     (userChoice === "rock" && comp === "scissor") ||
+//     (userChoice === "paper" && comp === "rock") ||
+//     (userChoice === "scissor" && comp === "paper")
+//     ? "You won" : "You lose";
 
-  document.getElementById("result").textContent = result;
-}
+//   document.getElementById("result").textContent = result;
+// }
 
-["rock","paper","scissor"].forEach(id =>
-  document.getElementById(id).onclick = () => playGame(id)
-);
+// ["rock","paper","scissor"].forEach(id =>
+//   document.getElementById(id).onclick = () => playGame(id)
+// );
+
+
+// one liner
+
+["rock","paper","scissor"].forEach(i=>document.getElementById(i).onclick=()=>{const c=["rock","paper","scissor"][Math.floor(Math.random()*3)];document.getElementById("myH1").textContent=`Computers Choice: ${c}`;document.getElementById("result").textContent=i===c?"It's a draw":(i==="rock"&&c==="scissor")||(i==="paper"&&c==="rock")||(i==="scissor"&&c==="paper")?"You won":"You lose";});
 
