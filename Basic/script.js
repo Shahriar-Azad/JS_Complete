c3d8c67687b7a7ae92d3706eb5f82670a7536f1f
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-12-25 23:52:39 +0600

    sleep function added

--- Basic/script.js
+++ Basic/script.js
@@ -851,33 +851,43 @@
 // console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // 6
 
 // Merge two sorted arrays into one sorted array
-function mergeSortedArrays(arr1, arr2) {
-    const merged = [];
-    let i = 0, j = 0;
+// function mergeSortedArrays(arr1, arr2) {
+//     const merged = [];
+//     let i = 0, j = 0;
     
-    while (i < arr1.length && j < arr2.length) {
-        if (arr1[i] < arr2[j]) {
-            merged.push(arr1[i]);
-            i++;
-        } else {
-            merged.push(arr2[j]);
-            j++;
-        }
-    }
+//     while (i < arr1.length && j < arr2.length) {
+//         if (arr1[i] < arr2[j]) {
+//             merged.push(arr1[i]);
+//             i++;
+//         } else {
+//             merged.push(arr2[j]);
+//             j++;
+//         }
+//     }
     
-    // Add remaining elements
-    while (i < arr1.length) {
-        merged.push(arr1[i]);
-        i++;
-    }
+//     // Add remaining elements
+//     while (i < arr1.length) {
+//         merged.push(arr1[i]);
+//         i++;
+//     }
     
-    while (j < arr2.length) {
-        merged.push(arr2[j]);
-        j++;
-    }
+//     while (j < arr2.length) {
+//         merged.push(arr2[j]);
+//         j++;
+//     }
     
-    return merged;
+//     return merged;
+// }
+
+// // Example
+// console.log(mergeSortedArrays([1, 3, 5], [2, 4, 6])); // [1, 2, 3, 4, 5, 6]
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// Example
-console.log(mergeSortedArrays([1, 3, 5], [2, 4, 6])); // [1, 2, 3, 4, 5, 6]
+(async () => {
+  console.log("Wait...");
+  await sleep(1000);
+  console.log("Done!");
+})();
