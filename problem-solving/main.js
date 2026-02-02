eaef06b272b6fa02fc35a54fbf966e387ebe7153
Shahriar-Azad <shahriar.azad123@gmail.com>
2026-02-02 23:39:49 +0600

    lru cache implementation

--- problem-solving/main.js
+++ problem-solving/main.js
@@ -297,23 +297,49 @@
 
 
 
-async function promisePool(tasks, limit = 3) {
-  const results = [];
-  const executing = [];
+// async function promisePool(tasks, limit = 3) {
+//   const results = [];
+//   const executing = [];
 
-  for (const task of tasks) {
-    const p = Promise.resolve().then(task);
-    results.push(p);
+//   for (const task of tasks) {
+//     const p = Promise.resolve().then(task);
+//     results.push(p);
 
-    if (limit <= tasks.length) {
-      const e = p.then(() => executing.splice(executing.indexOf(e), 1));
-      executing.push(e);
+//     if (limit <= tasks.length) {
+//       const e = p.then(() => executing.splice(executing.indexOf(e), 1));
+//       executing.push(e);
 
-      if (executing.length >= limit) {
-        await Promise.race(executing);
-      }
-    }
+//       if (executing.length >= limit) {
+//         await Promise.race(executing);
+//       }
+//     }
+//   }
+
+//   return Promise.all(results);
+// }
+
+
+
+class LRUCache {
+  constructor(limit = 5) {
+    this.limit = limit;
+    this.cache = new Map();
   }
 
-  return Promise.all(results);
+  get(key) {
+    if (!this.cache.has(key)) return null;
+    const val = this.cache.get(key);
+    this.cache.delete(key);
+    this.cache.set(key, val);
+    return val;
+  }
+
+  put(key, value) {
+    if (this.cache.has(key)) this.cache.delete(key);
+    else if (this.cache.size >= this.limit) {
+      const firstKey = this.cache.keys().next().value;
+      this.cache.delete(firstKey);
+    }
+    this.cache.set(key, value);
+  }
 }
