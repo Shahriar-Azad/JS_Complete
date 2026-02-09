43d82207591b69c3dd52d1d5fea270b5eaeaffe3
Shahriar-Azad <shahriar.azad123@gmail.com>
2026-02-09 23:56:13 +0600

    binary search

--- problem-solving/main.js
+++ problem-solving/main.js
@@ -536,12 +536,28 @@
 // }
 
 
-function flattenDeep(arr) {
-  return arr.reduce(
-    (acc, val) =>
-      Array.isArray(val) ? acc.concat(flattenDeep(val)) : acc.concat(val),
-    []
-  );
+// function flattenDeep(arr) {
+//   return arr.reduce(
+//     (acc, val) =>
+//       Array.isArray(val) ? acc.concat(flattenDeep(val)) : acc.concat(val),
+//     []
+//   );
+// }
+
+
+function binarySearch(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (arr[mid] === target) return mid;
+    if (arr[mid] < target) left = mid + 1;
+    else right = mid - 1;
+  }
+
+  return -1;
 }
+
 
 
