819e1bcd2194a74b43aa487268ad1d7cca2504db
Shahriar-Azad <shahriar.azad123@gmail.com>
2026-02-13 23:47:23 +0600

    shuffle array

--- problem-solving/main.js
+++ problem-solving/main.js
@@ -651,6 +651,16 @@
 // }
 
 
-function capitalizeWords(str) {
-  return str.replace(/\b\w/g, char => char.toUpperCase());
+// function capitalizeWords(str) {
+//   return str.replace(/\b\w/g, char => char.toUpperCase());
+// }
+
+
+function shuffleArray(arr) {
+  const result = [...arr];
+  for (let i = result.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [result[i], result[j]] = [result[j], result[i]];
+  }
+  return result;
 }
