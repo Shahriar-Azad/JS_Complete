e64d255734be42c4c0b97240d94f3e450fcdf5aa
Shahriar-Azad <shahriar.azad123@gmail.com>
2026-02-15 23:58:18 +0600

    format bytes

--- problem-solving/main.js
+++ problem-solving/main.js
@@ -679,14 +679,28 @@
 // }
 
 
-function createLogger(level = "info") {
-  const levels = ["debug", "info", "warn", "error"];
-  const currentLevel = levels.indexOf(level);
+// function createLogger(level = "info") {
+//   const levels = ["debug", "info", "warn", "error"];
+//   const currentLevel = levels.indexOf(level);
 
-  return {
-    debug: (...args) => currentLevel <= 0 && console.debug(...args),
-    info: (...args) => currentLevel <= 1 && console.info(...args),
-    warn: (...args) => currentLevel <= 2 && console.warn(...args),
-    error: (...args) => currentLevel <= 3 && console.error(...args),
-  };
+//   return {
+//     debug: (...args) => currentLevel <= 0 && console.debug(...args),
+//     info: (...args) => currentLevel <= 1 && console.info(...args),
+//     warn: (...args) => currentLevel <= 2 && console.warn(...args),
+//     error: (...args) => currentLevel <= 3 && console.error(...args),
+//   };
+// }
+
+
+function formatBytes(bytes, decimals = 2) {
+  if (bytes === 0) return "0 Bytes";
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
 }
+
