045b7596858677dd143e7b0c5accb1f7f50384c8
Shahriar-Azad <shahriar.azad123@gmail.com>
2026-02-16 23:58:56 +0600

    escape html

--- problem-solving/main.js
+++ problem-solving/main.js
@@ -692,15 +692,27 @@
 // }
 
 
-function formatBytes(bytes, decimals = 2) {
-  if (bytes === 0) return "0 Bytes";
+// function formatBytes(bytes, decimals = 2) {
+//   if (bytes === 0) return "0 Bytes";
 
-  const k = 1024;
-  const dm = decimals < 0 ? 0 : decimals;
-  const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+//   const k = 1024;
+//   const dm = decimals < 0 ? 0 : decimals;
+//   const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
 
-  const i = Math.floor(Math.log(bytes) / Math.log(k));
+//   const i = Math.floor(Math.log(bytes) / Math.log(k));
 
-  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
+//   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
+// }
+
+
+function escapeHTML(str) {
+  const map = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&#039;",
+  };
+
+  return str.replace(/[&<>"']/g, m => map[m]);
 }
-
