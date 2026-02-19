a44794db852e2a09813cbac2617a6770453e9254
Shahriar-Azad <shahriar.azad123@gmail.com>
2026-02-19 23:59:29 +0600

    two sum:

--- problem-solving/main.js
+++ problem-solving/main.js
@@ -777,6 +777,20 @@
 // }
 
 
-function compact(arr) {
-  return arr.filter(Boolean);
+// function compact(arr) {
+//   return arr.filter(Boolean);
+// }
+
+function twoSum(nums, target) {
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const needed = target - nums[i];
+
+    if (map.has(needed)) {
+      return [map.get(needed), i];
+    }
+
+    map.set(nums[i], i);
+  }
 }
