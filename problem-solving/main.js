fb6198bf18655fd7d2341b9074ef002ca115f1e1
Shahriar-Azad <shahriar.azad123@gmail.com>
2026-02-21 22:22:14 +0600

    middlenode

--- problem-solving/main.js
+++ problem-solving/main.js
@@ -858,19 +858,32 @@
 // }
 
 
-function lengthOfLongestSubstring(s) {
-  let left = 0;
-  let maxLen = 0;
-  const set = new Set();
+// function lengthOfLongestSubstring(s) {
+//   let left = 0;
+//   let maxLen = 0;
+//   const set = new Set();
 
-  for (let right = 0; right < s.length; right++) {
-    while (set.has(s[right])) {
-      set.delete(s[left]);
-      left++;
-    }
-    set.add(s[right]);
-    maxLen = Math.max(maxLen, right - left + 1);
+//   for (let right = 0; right < s.length; right++) {
+//     while (set.has(s[right])) {
+//       set.delete(s[left]);
+//       left++;
+//     }
+//     set.add(s[right]);
+//     maxLen = Math.max(maxLen, right - left + 1);
+//   }
+
+//   return maxLen;
+// }
+
+
+function middleNode(head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
   }
 
-  return maxLen;
+  return slow;
 }
