b37f087c34a8797e984b6f10eb3119469aff1f3c
Shahriar-Azad <shahriar.azad123@gmail.com>
2026-02-28 00:00:34 +0600

    num island

--- problem-solving/main.js
+++ problem-solving/main.js
@@ -1124,29 +1124,59 @@
 //   return count;
 // }
 
-function search(nums, target) {
-  let left = 0;
-  let right = nums.length - 1;
+// function search(nums, target) {
+//   let left = 0;
+//   let right = nums.length - 1;
 
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-    if (nums[mid] === target) return mid;
+//   while (left <= right) {
+//     const mid = Math.floor((left + right) / 2);
+//     if (nums[mid] === target) return mid;
 
-    if (nums[left] <= nums[mid]) {
-      if (target >= nums[left] && target < nums[mid]) {
-        right = mid - 1;
-      } else {
-        left = mid + 1;
-      }
-    } else {
-      if (target > nums[mid] && target <= nums[right]) {
-        left = mid + 1;
-      } else {
-        right = mid - 1;
+//     if (nums[left] <= nums[mid]) {
+//       if (target >= nums[left] && target < nums[mid]) {
+//         right = mid - 1;
+//       } else {
+//         left = mid + 1;
+//       }
+//     } else {
+//       if (target > nums[mid] && target <= nums[right]) {
+//         left = mid + 1;
+//       } else {
+//         right = mid - 1;
+//       }
+//     }
+//   }
+
+//   return -1;
+// }
+
+
+function numIslands(grid) {
+  if (!grid.length) return 0;
+  let count = 0;
+
+  function dfs(r, c) {
+    if (
+      r < 0 || c < 0 ||
+      r >= grid.length || c >= grid[0].length ||
+      grid[r][c] === "0"
+    ) return;
+
+    grid[r][c] = "0";
+    dfs(r + 1, c);
+    dfs(r - 1, c);
+    dfs(r, c + 1);
+    dfs(r, c - 1);
+  }
+
+  for (let r = 0; r < grid.length; r++) {
+    for (let c = 0; c < grid[0].length; c++) {
+      if (grid[r][c] === "1") {
+        count++;
+        dfs(r, c);
       }
     }
   }
 
-  return -1;
+  return count;
 }
-
