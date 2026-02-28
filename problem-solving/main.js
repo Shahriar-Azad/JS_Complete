ea2256207f357a77112b32599f477e66f345c633
Shahriar-Azad <shahriar.azad123@gmail.com>
2026-02-28 23:51:43 +0600

    find kth largest

--- problem-solving/main.js
+++ problem-solving/main.js
@@ -1151,32 +1151,39 @@
 // }
 
 
-function numIslands(grid) {
-  if (!grid.length) return 0;
-  let count = 0;
+// function numIslands(grid) {
+//   if (!grid.length) return 0;
+//   let count = 0;
 
-  function dfs(r, c) {
-    if (
-      r < 0 || c < 0 ||
-      r >= grid.length || c >= grid[0].length ||
-      grid[r][c] === "0"
-    ) return;
+//   function dfs(r, c) {
+//     if (
+//       r < 0 || c < 0 ||
+//       r >= grid.length || c >= grid[0].length ||
+//       grid[r][c] === "0"
+//     ) return;
 
-    grid[r][c] = "0";
-    dfs(r + 1, c);
-    dfs(r - 1, c);
-    dfs(r, c + 1);
-    dfs(r, c - 1);
-  }
+//     grid[r][c] = "0";
+//     dfs(r + 1, c);
+//     dfs(r - 1, c);
+//     dfs(r, c + 1);
+//     dfs(r, c - 1);
+//   }
 
-  for (let r = 0; r < grid.length; r++) {
-    for (let c = 0; c < grid[0].length; c++) {
-      if (grid[r][c] === "1") {
-        count++;
-        dfs(r, c);
-      }
-    }
-  }
+//   for (let r = 0; r < grid.length; r++) {
+//     for (let c = 0; c < grid[0].length; c++) {
+//       if (grid[r][c] === "1") {
+//         count++;
+//         dfs(r, c);
+//       }
+//     }
+//   }
 
-  return count;
+//   return count;
+// }
+
+
+
+function findKthLargest(nums, k) {
+  nums.sort((a, b) => b - a);
+  return nums[k - 1];
 }
